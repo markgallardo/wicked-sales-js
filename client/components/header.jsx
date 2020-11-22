f5b1c44c24a3daa28d515d941846fc803712e820
markgallardo <markgallardo1221@gmail.com>
2020-11-21 21:13:05 -0800

    favicon and feedback

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header className="Sticky-top align-items-center mb-3 mt-3  justify-content-between">
-      <h2 className="ml-3 mb-0 header-font cursor" onClick={() => { props.setView('catalog', {}); }}><img src="/images/dota2icon.svg" alt="" className="header-logo"/>The Secret Shop</h2>
+      <h2 className="ml-3 mb-0 header-font cursor logo" onClick={() => { props.setView('catalog', {}); }}><img src="/images/dota2icon.svg" alt="" className="header-logo"/>The Secret Shop</h2>
       <div className="row align-items-center justify-content-end mr-4 cursor" onClick={() => { props.setView('cart', {}); }}>
         <p className="text-light mb-0">{props.cartItemCount} Items <i className="fas fa-shopping-cart text-light"></i></p>
       </div>
