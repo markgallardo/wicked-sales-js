529eec831bf7d3b2150ae26fb0c3eb64e9154dc6
markgallardo <markgallardo1221@gmail.com>
2020-11-25 19:34:59 -0800

    feedback done and removebutton added

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,11 +4,7 @@ export default function ProductListItem(props) {
 
   return (
     <div className="card mb-4 card-hover cardItem product-img" onClick={() => props.setView('details', { productId: props.productId })}>
-      <div className="column">
-        <figure>
-          <img src={props.products.image} alt={props.products.name} className="card-img-top"/>
-        </figure>
-      </div>
+      <img src={props.products.image} alt={props.products.name} className="card-img-top"/>
       <div className="card-body">
         <h5 className="card-title">{props.products.name}</h5>
         <p className="card-text text-muted"><strong>{PriceFormat(props.products.price)}</strong></p>
